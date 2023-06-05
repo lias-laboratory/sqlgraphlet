079c10c281d5f7b7c7cef8c5b333a716277af0af
Abir FAROUZI <69763223+FarouziAbir@users.noreply.github.com>
2023-06-05 11:29:56 +0200

    Add sqlgraphlet project

--- sqlgraphlet.py
+++ sqlgraphlet.py
@@ -1,3 +1,27 @@
+#!/usr/bin/env python
+""" This module is for graphlet enumeration by stages in a distributed fashion using SQL queries.
+
+This software is provided by the copyright holders and contributors "as is" 
+and any express or implied warranties, including, but not limited to, the
+implied warranties of merchantability and fitness for a particular purpose are
+disclaimed. in no event shall the copyright holder or contributors be liable
+for any direct, indirect, incidental, special, exemplary, or consequential
+damages (including, but not limited to, procurement of substitute goods or
+services; loss of use, data, or profits; or business interruption) however
+caused and on any theory of liability, whether in contract, strict liability,
+or tort (including negligence or otherwise) arising in any way out of the use
+of this software, even if advised of the possibility of such damage.
+
+"""
+
+__author__ = "Abir Farouzi"
+__contact__ = "farouzi.abir@gmail.com"
+__copyright__ = "Copyright 2023, LIAS at ISAE/ENSMA"
+__date__ = "2023/06/05"
+__deprecated__ = False
+__license__ = "BSD-3"
+__maintainer__ = "developer"
+__version__ = "1.0"
 
 import vertica_python as vp
 import pandas as pd
